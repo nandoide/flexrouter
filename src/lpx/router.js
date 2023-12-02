6c53980ae135a42b803debf846e5d44199f7f975
nandoide <nangdoide@gmail.com>
2023-12-02 10:06:40 +0100

    octaves

--- src/lpx/router.js
+++ src/lpx/router.js
@@ -8,6 +8,13 @@ const scaler_performance = 10;
 const scaler_cc_performance = 60;
 const scaler_cc_performance_limit = 9;
 
+const intervals = "2212221" // major scale
+const notes_range_ini = 21; //A-1, firs white after 8 chords in scaler
+const root = 24; // C0
+const octave_up = 20; // octave up from 0 to octaves - 1
+const octaves = [1,2,3]; // octaves up and down
+let octave = 0; // octave index in octaves
+
 function send_cc(index, root_cc) {
     Trace("Send CC");
     if (root_cc == undefined) {
@@ -39,6 +46,24 @@ function HandleMIDI(event) {
             event.velocity = scaler_cc_performance_limit;
         }
         send_cc(event.velocity, scaler_cc_performance);
+    } else if (event instanceof NoteOn && event.pitch == octave_up) {
+        if (DEBUG) Trace("Octave change")
+        if (DEBUG) Trace(octave);
+        octave = (octave + 1) % octaves.length;
+        if (DEBUG) Trace(octave);
+    } else if ((event instanceof NoteOn || event instanceof NoteOff) && event.pitch >= notes_range_ini) {
+        //Modify pitch to white key from C3 (default) and mapping to white keys
+        Trace('Displace octave', event.pitch);
+        if (DEBUG) event.trace();
+        var pitch_index = event.pitch - notes_range_ini;
+        var pitch = root + octaves[octave] * 12;
+        let intervals_len = intervals.length;
+        for (var i = 0; i < pitch_index; i++) {
+            pitch += parseInt(intervals[i % intervals_len]);
+        }
+        event.pitch = pitch;
+        if (DEBUG) event.trace();
+        event.send();
     } else {
         if (DEBUG) Trace("Not intercepted");
         if (DEBUG) event.trace();
